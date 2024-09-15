f7cec4dcddc09c2e328f809ae2ad16f41b78dbeb
amaanrzv39 <amaan.rizvi39@gmail.com>
2024-09-15 19:18:31 +0530

    deploy to ghcr

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ LOG.setLevel(logging.INFO)
 
 @app.route("/")
 def home():
-    html = f"<h3>Predict the Height From Weight of MLB Players. CD</h3>"
+    html = f"<h3>This app predicts the Height From Weight of MLB Players. CD</h3>"
     return html.format(format)
 
 
